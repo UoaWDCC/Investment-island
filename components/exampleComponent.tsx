be6ce8cc6334c390832ec3b51eaf6e5032fc55df
lukisoo <84960461+lukisoo@users.noreply.github.com>
2022-04-26 03:51:51 +1200

    Added some example files

---
+++ components/exampleComponent.tsx
@@ -0,0 +1,12 @@
+/* Imports go at the top of the file */
+import Head from "next/head";
+
+export default function ExampleComponent() {
+  return (
+    <div className="content">
+
+        {/* Whatever goes here */}
+
+    </div>
+  );
+}
---
+++ pages/example/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+/* Import the component here in order to use it */
+import ExampleComponent from '../../components/exampleComponent'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+
+        <ExampleComponent/>
+
+    </div>
+  )
+}
+
+export default NextPage
