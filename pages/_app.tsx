2f8321cde3ffaeab75e3adf4c7fbaa32561f24b0
Brendan-Z <brendanzhou.99@gmail.com>
2022-05-20 20:04:40 +1200

    [II-08] Implemented firebase initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { readData } from '../firebase/data'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  readData().then(data => console.log(data));
   return <Component {...pageProps} />
 }
 
