0d3309651838fcdd5bb3843742a499baf0df3412
Youxiang <ylei532@aucklanduni.ac.nz>
2022-07-15 17:37:04 +1200

    logout finished
    Co-authored-by: Daniel Torrey <danieltorrey@users.noreply.github.com>

---
+++ firebase/userSignUp.js
@@ -0,0 +1,40 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+} from "firebase/auth";
+
+export default async function createUser(email, password) {
+  try {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+export async function createUserGoogle() {
+  try {
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+    await signInWithPopup(auth, googleProvider);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+export async function createFacebookUser() {
+  try {
+    const auth = getAuth();
+    const facebookProvider = new FacebookAuthProvider();
+    signInWithPopup(auth, facebookProvider);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
